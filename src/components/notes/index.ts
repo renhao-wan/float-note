e6a0b09d903b4b0f4efd13efc86b4448dcecd0a1
wan <2653990378@qq.com>
2026-06-06 20:45:59 +0800

    refactor: 删除未使用的遗留组件 NoteEditor
    - 删除 src/components/notes/NoteEditor.tsx
    - 从 index.ts 移除导出
    - 该组件使用 React Query 而非主应用的 useNoteManagement
    - 实际使用的是 src/components/editor/NoteEditor.tsx

--- src/components/notes/index.ts
+++ src/components/notes/index.ts
@@ -1,5 +1,4 @@
 export { EditorArea } from './EditorArea';
 export { EmptyState } from './EmptyState';
-export { NoteEditor } from './NoteEditor';
 export { NotesPanel } from './NotesPanel';
 export { TitleEditor } from './TitleEditor';
